03a1751af1634447282eb22b65d1f0f66f7efa3a
brunorg21 <rbrunog21@gmail.com>
2024-03-28 09:28:15 -0300

    feat: add env setup

---
+++ src/env/index.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+const envSchmea = z.object({
+  NODE_ENV: z.enum(["dev", "production"]),
+  PORT: z.coerce.number(),
+});
+
+const _env = envSchmea.safeParse(process.env);
+
+if (!_env.success) {
+  console.error("Invalid environment variables", _env.error.format());
+
+  throw new Error("Invalid environment variables.");
+}
+
+export const env = _env.data;
