afe8bcec025ef367b3802960c58710a2995c0e9c
brunorg21 <rbrunog21@gmail.com>
2024-04-11 23:09:11 -0300

    feat: add find-unique-organization use case

---
+++ src/use-cases/organization/find-unique-organization.spec.ts
@@ -0,0 +1,31 @@
+import { InMemoryOrganizationRepository } from "@/repositories/in-memory-repositories/in-memory-organization-repository";
+import { beforeEach, describe, expect, it } from "vitest";
+import { FindUniqueOrganizationUseCase } from "./find-unique-organization";
+import { ResourceNotFoundError } from "../errors/resource-not-found-error";
+
+let inMemoryOrganizationRepository: InMemoryOrganizationRepository;
+let sut: FindUniqueOrganizationUseCase;
+
+beforeEach(() => {
+  inMemoryOrganizationRepository = new InMemoryOrganizationRepository();
+  sut = new FindUniqueOrganizationUseCase(inMemoryOrganizationRepository);
+});
+
+describe("find organization by id use case", () => {
+  it("should be able to find organization", async () => {
+    const organizationCreated = await inMemoryOrganizationRepository.create({
+      createdAt: new Date(),
+      name: "Organization 1",
+    });
+
+    const organization = await sut.execute(organizationCreated.id);
+
+    expect(organization.name).toEqual("Organization 1");
+  });
+
+  it("should not be able to find organization with wrong id", async () => {
+    await expect(async () => {
+      await sut.execute("wrong-id");
+    }).rejects.toBeInstanceOf(ResourceNotFoundError);
+  });
+});
---
+++ src/use-cases/organization/find-unique-organization.ts
@@ -0,0 +1,16 @@
+import { OrganizationRepository } from "@/repositories/organization-repository";
+import { ResourceNotFoundError } from "../errors/resource-not-found-error";
+
+export class FindUniqueOrganizationUseCase {
+  constructor(private organizationRepository: OrganizationRepository) {}
+
+  async execute(id: string) {
+    const organization = await this.organizationRepository.findById(id);
+
+    if (!organization) {
+      throw new ResourceNotFoundError();
+    }
+
+    return organization;
+  }
+}
