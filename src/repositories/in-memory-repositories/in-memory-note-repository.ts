ceaadd701163631892ab8b12ba82b1437155e796
brunorg21 <rbrunog21@gmail.com>
2024-04-16 09:17:46 -0300

    feat: add in-memory-note-repository

---
+++ src/repositories/in-memory-repositories/in-memory-note-repository.ts
@@ -0,0 +1,43 @@
+import { INoteCreate, INote } from "@/models/note-model";
+import { NoteRepository } from "../note-repository";
+import { randomUUID } from "crypto";
+
+export class InMemoryNoteRepository implements NoteRepository {
+  public items: INote[] = [];
+
+  async create(data: INoteCreate): Promise<INote> {
+    const org = {
+      id: randomUUID(),
+      authorId: data.authorId,
+      createdAt: new Date(),
+      description: data.description,
+      taskId: data.taskId,
+    } as INote;
+
+    this.items.push(org);
+
+    return org;
+  }
+
+  async findById(id: string): Promise<INote | null> {
+    const note = this.items.find((note) => note.id === id);
+
+    if (!note) {
+      return null;
+    }
+
+    return note;
+  }
+
+  async delete(id: string): Promise<void> {
+    const itemIndex = this.items.findIndex((item) => item.id === id);
+
+    this.items.splice(itemIndex, 1);
+  }
+
+  async save(note: INote): Promise<void> {
+    const noteIndex = this.items.findIndex((item) => item.id === note.id);
+
+    this.items[noteIndex] = note;
+  }
+}
--- src/repositories/note-repository.ts
+++ src/repositories/note-repository.ts
@@ -1,3 +1,8 @@
+import { INote, INoteCreate } from "@/models/note-model";
+
 export interface NoteRepository {
-  create(): Promise<void>;
+  create(data: INoteCreate): Promise<INote>;
+  save(note: INote): Promise<void>;
+  delete(noteId: string): Promise<void>;
+  findById(noteId: string): Promise<INote | null>;
 }
