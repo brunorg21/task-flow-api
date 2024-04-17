325ef6b7b43f5e11856ffddc7fc37dc3e70765d6
brunorg21 <rbrunog21@gmail.com>
2024-04-17 19:35:32 -0300

    feat: add delete use case and unit test

---
+++ src/use-cases/note/delete.spec.ts
@@ -0,0 +1,30 @@
+import { InMemoryNoteRepository } from "@/repositories/in-memory-repositories/in-memory-note-repository";
+import { beforeEach, describe, expect, it } from "vitest";
+import { DeleteNoteUseCase } from "./delete";
+import { makeNote } from "../factories/make-note";
+import { ResourceNotFoundError } from "../errors/resource-not-found-error";
+
+let inMemoryNoteRepository: InMemoryNoteRepository;
+
+let sut: DeleteNoteUseCase;
+
+beforeEach(() => {
+  inMemoryNoteRepository = new InMemoryNoteRepository();
+  sut = new DeleteNoteUseCase(inMemoryNoteRepository);
+});
+
+describe("delete note use case", () => {
+  it("should be able to delete an note", async () => {
+    const newNote = await makeNote(inMemoryNoteRepository);
+
+    await sut.execute(newNote.id);
+
+    expect(inMemoryNoteRepository.items).toHaveLength(0);
+  });
+
+  it("should not be able to delete an note with wrong id", async () => {
+    expect(async () => await sut.execute("note-4")).rejects.toBeInstanceOf(
+      ResourceNotFoundError
+    );
+  });
+});
---
+++ src/use-cases/note/delete.ts
@@ -0,0 +1,16 @@
+import { NoteRepository } from "@/repositories/note-repository";
+import { ResourceNotFoundError } from "../errors/resource-not-found-error";
+
+export class DeleteNoteUseCase {
+  constructor(private noteRepository: NoteRepository) {}
+
+  async execute(id: string) {
+    const note = await this.noteRepository.findById(id);
+
+    if (!note) {
+      throw new ResourceNotFoundError();
+    }
+
+    await this.noteRepository.delete(note.id);
+  }
+}
