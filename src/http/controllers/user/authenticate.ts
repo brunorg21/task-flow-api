8336c50c8727dfe5f573ad379c7c24617610ee6a
brunorg21 <rbrunog21@gmail.com>
2024-04-20 11:07:02 -0300

    feat: add authenticate controller

---
+++ src/http/controllers/user/authenticate.ts
@@ -0,0 +1,38 @@
+import { makeAuthenticateUseCase } from "@/http/factories/make-authenticate-use-case";
+import { InvalidCredentialsError } from "@/use-cases/errors/invalid-credentials";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+export async function authenticate(req: FastifyRequest, reply: FastifyReply) {
+  const authenticateRequestBodySchema = z.object({
+    email: z.string().email(),
+    password: z.string(),
+  });
+
+  const { email, password } = authenticateRequestBodySchema.parse(req.body);
+
+  const authenticateUseCase = makeAuthenticateUseCase();
+
+  try {
+    const { user } = await authenticateUseCase.execute({
+      email,
+      password,
+    });
+
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
+      }
+    );
+    return reply.status(200).send({ token });
+  } catch (error) {
+    if (error instanceof InvalidCredentialsError) {
+      return reply.status(400).send({
+        message: error.message,
+      });
+    }
+  }
+}
