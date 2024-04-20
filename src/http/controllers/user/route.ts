ae5e040a7672c5041dd0dff7b656833c53e25c6f
brunorg21 <rbrunog21@gmail.com>
2024-04-20 11:07:24 -0300

    feat: add authenticate route

--- src/http/controllers/user/route.ts
+++ src/http/controllers/user/route.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from "fastify";
 import { create } from "./create";
+import { authenticate } from "./authenticate";
 
 export async function userRoutes(app: FastifyInstance) {
   app.post("/user", create);
+  app.post("/authenticate", authenticate);
 }
