6b490468d9ba1b923dfbe07f58b5bf897bd617f1
brunorg21 <rbrunog21@gmail.com>
2024-04-21 19:40:02 -0300

    feat: add get profile controller

---
+++ src/http/controllers/user/profile.ts
@@ -0,0 +1,24 @@
+import { makeProfileUseCase } from "@/http/factories/make-profile-use-case";
+import { InvalidCredentialsError } from "@/use-cases/errors/invalid-credentials";
+import { FastifyReply, FastifyRequest } from "fastify";
+
+export async function profile(req: FastifyRequest, reply: FastifyReply) {
+  const getProfileUseCase = makeProfileUseCase();
+
+  try {
+    const { user } = await getProfileUseCase.execute({
+      userId: req.user.sub,
+    });
+
+    return reply.status(200).send({
+      ...user,
+      password: undefined,
+    });
+  } catch (error) {
+    if (error instanceof InvalidCredentialsError) {
+      return reply.status(401).send({
+        message: error.message,
+      });
+    }
+  }
+}
