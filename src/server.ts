33a526d1f80f8939793d63d54f33f711545d5fbc
brunorg21 <rbrunog21@gmail.com>
2024-04-23 14:14:29 -0300

    feat: add userOrganizationRoutes on server

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,14 @@ import fastify from "fastify";
 import { env } from "./env";
 import { userRoutes } from "./http/controllers/user/route";
 import fastifyJwt from "@fastify/jwt";
+import { userOrganizationRoutes } from "./http/controllers/user-organization/route";
 
 const app = fastify();
 app.register(fastifyJwt, {
   secret: env.SECRET_KEY,
 });
 app.register(userRoutes);
+app.register(userOrganizationRoutes);
 
 app
   .listen({
