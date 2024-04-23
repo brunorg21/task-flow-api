126ba313a36f9bf8da0c60f5cf162bf40d4a5053
brunorg21 <rbrunog21@gmail.com>
2024-04-23 14:29:09 -0300

    feat: add edit attachment use case and unit test

---
+++ src/use-cases/attachments/edit.spec.ts
@@ -0,0 +1,32 @@
+import { InMemoryAttachmentRepository } from "@/repositories/in-memory-repositories/in-memory-attachment-repository";
+import { beforeEach, describe, expect, it } from "vitest";
+import { EditAttachmentUseCase } from "./edit";
+
+let inMemoryAttachmentRepository: InMemoryAttachmentRepository;
+
+let sut: EditAttachmentUseCase;
+
+beforeEach(() => {
+  inMemoryAttachmentRepository = new InMemoryAttachmentRepository();
+  sut = new EditAttachmentUseCase(inMemoryAttachmentRepository);
+});
+
+describe("edit attachment use case", () => {
+  it("should be able to edit an attachment", async () => {
+    const attachment = await inMemoryAttachmentRepository.createMany([
+      {
+        createdAt: new Date(),
+        fileName: "teste",
+        noteId: null,
+        taskId: null,
+        url: "teste",
+      },
+    ]);
+
+    attachment[0].taskId = "1";
+
+    await sut.execute(attachment);
+
+    expect(attachment[0].taskId).toEqual(expect.any(String));
+  });
+});
