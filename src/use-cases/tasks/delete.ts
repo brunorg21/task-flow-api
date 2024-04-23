e23715352715308d0c5c2565af50374695f0c220
brunorg21 <rbrunog21@gmail.com>
2024-04-23 14:51:23 -0300

    feat: add deleteMany attachments on task use case

--- src/use-cases/tasks/delete.ts
+++ src/use-cases/tasks/delete.ts
@@ -3,13 +3,20 @@ import { ResourceNotFoundError } from "../@errors/resource-not-found-error";
 import { AttachmentRepository } from "@/repositories/attachment-repository";
 
 export class DeleteTaskUseCase {
-  constructor(private taskRepository: TaskRepository) {}
+  constructor(
+    private taskRepository: TaskRepository,
+    private attachmentRepository: AttachmentRepository
+  ) {}
 
   async execute(id: string) {
     const task = await this.taskRepository.findById(id);
 
     if (!task) {
       throw new ResourceNotFoundError();
+    }
+
+    if (task.attachments) {
+      await this.attachmentRepository.deleteMany(task.attachments);
     }
 
     await this.taskRepository.delete(task.id);
