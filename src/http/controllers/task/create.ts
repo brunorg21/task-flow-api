ecbc065d33c6f50f8110a70033a170c4fda23e43
brunorg21 <rbrunog21@gmail.com>
2024-04-26 15:51:38 -0300

    feat: add create route

---
+++ src/http/controllers/task/create.ts
@@ -0,0 +1,35 @@
+import { makeCreateTaskUseCase } from "@/http/factories/make-create-task-use-case";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+export async function create(req: FastifyRequest, reply: FastifyReply) {
+  const createTaskRequestBodySchema = z.object({
+    title: z.string(),
+    organizationId: z.string().uuid().nullable().default(null),
+    assignedId: z.string().uuid().nullable().default(null),
+    status: z.enum(["Em andamento", "Concluída", "Cancelada"]),
+    attachments: z.array(z.string()).nullable().default(null),
+  });
+
+  const { assignedId, organizationId, status, title, attachments } =
+    createTaskRequestBodySchema.parse(req.body);
+
+  const createTaskUseCase = makeCreateTaskUseCase();
+
+  try {
+    await createTaskUseCase.execute({
+      assignedId,
+      attachments,
+      organizationId,
+      title,
+      userId: req.user.sub,
+      status,
+    });
+
+    return reply.status(201).send();
+  } catch (error) {
+    return reply.status(400).send({
+      message: "Não foi possível criar",
+    });
+  }
+}
