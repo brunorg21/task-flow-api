ba4fd9456e271d411fd22488edfe90aa80fa13ce
brunorg21 <rbrunog21@gmail.com>
2024-04-26 15:52:33 -0300

    feat: add fastifyMultipart on fastify

--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,27 @@ import { env } from "./env";
 import { userRoutes } from "./http/controllers/user/route";
 import fastifyJwt from "@fastify/jwt";
 import { userOrganizationRoutes } from "./http/controllers/user-organization/route";
+import { taskRoutes } from "./http/controllers/task/route";
+import fastifyMultipart from "@fastify/multipart";
+import { attachmentRoutes } from "./http/controllers/attachment/route";
 
 const app = fastify();
+
 app.register(fastifyJwt, {
   secret: env.SECRET_KEY,
 });
+
+app.register(fastifyMultipart, {
+  limits: {
+    fileSize: 5000000,
+  },
+});
+
+//Routes
 app.register(userRoutes);
 app.register(userOrganizationRoutes);
+app.register(taskRoutes);
+app.register(attachmentRoutes);
 
 app
   .listen({
