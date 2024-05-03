7e9c481e85ed91072757adbc35f6d9f735538072
brunorg21 <rbrunog21@gmail.com>
2024-05-02 22:14:26 -0300

    remove: remove import

--- src/http/factories/make-edit-task.use-case.ts
+++ src/http/factories/make-edit-task.use-case.ts
@@ -1,7 +1,6 @@
 import { DrizzleAttachmentRepository } from "@/repositories/drizzle-repositories/drizzle-attachment-repository";
 import { DrizzleTaskRepository } from "@/repositories/drizzle-repositories/drizzle-task-repository";
 import { EditTaskUseCase } from "@/use-cases/tasks/edit";
-import { FindTaskByIdUseCase } from "@/use-cases/tasks/find-task-by-id";
 
 export function makeEditTaskUseCase() {
   const drizzleAttachmentRepository = new DrizzleAttachmentRepository();
