62d79f37d57b7e5dbeaa8e88b2323de27bea6a1a
brunorg21 <rbrunog21@gmail.com>
2024-05-02 21:38:32 -0300

    feat: add new routes to task

--- src/http/controllers/task/route.ts
+++ src/http/controllers/task/route.ts
@@ -1,9 +1,15 @@
 import { jwtVerify } from "@/http/middlewares/jwt-verify";
 import { FastifyInstance } from "fastify";
 import { create } from "./create";
+import { findById } from "./find-by-id";
+import { edit } from "./edit";
+import { deleteTask } from "./delete";
 
 export async function taskRoutes(app: FastifyInstance) {
   app.addHook("onRequest", jwtVerify);
 
   app.post("/task", create);
+  app.get("/task/:id", findById);
+  app.put("/task/:id", edit);
+  app.delete("/task/:id", deleteTask);
 }
