700774d7ad3e31878aaf2283108657656ef2ecce
brunorg21 <rbrunog21@gmail.com>
2024-05-02 21:42:48 -0300

    feat: add delete images on storage on delete task

--- src/use-cases/tasks/delete.ts
+++ src/use-cases/tasks/delete.ts
@@ -1,6 +1,7 @@
 import { TaskRepository } from "@/repositories/task-repository";
 import { ResourceNotFoundError } from "../@errors/resource-not-found-error";
 import { AttachmentRepository } from "@/repositories/attachment-repository";
+import { supabaseClient } from "@/db/supabase/client";
 
 export class DeleteTaskUseCase {
   constructor(
@@ -14,9 +15,23 @@ export class DeleteTaskUseCase {
     if (!task) {
       throw new ResourceNotFoundError();
     }
+    const attachments = await this.attachmentRepository.findManyByTaskId(
+      task.id
+    );
 
-    if (task.attachments) {
-      await this.attachmentRepository.deleteMany(task.attachments);
+    if (attachments) {
+      await Promise.all(
+        attachments.map(
+          async (attachment) =>
+            await supabaseClient.storage
+              .from("attachments")
+              .remove([attachment.fileName])
+        )
+      );
+
+      await this.attachmentRepository.deleteMany(
+        attachments.map((attachment) => attachment.id)
+      );
     }
 
     await this.taskRepository.delete(task.id);
