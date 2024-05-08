6490ca621561d48a2f746f1567b124777d2cbe04
brunorg21 <rbrunog21@gmail.com>
2024-05-08 13:55:45 -0300

    feat: add more props on interface repository

--- src/repositories/task-repository.ts
+++ src/repositories/task-repository.ts
@@ -1,13 +1,19 @@
-import { ITask, ITaskCreate } from "@/models/task-model";
+import { ITask, ITaskCreate, ITaskList } from "@/models/task-model";
 
 export interface TaskRepository {
   create(data: ITaskCreate): Promise<void>;
   save(task: ITask): Promise<void>;
   delete(id: string): Promise<void>;
   findById(taskId: string): Promise<ITask | null>;
-  findManyByUser(userId: string): Promise<ITask[] | null>;
-  findManyByOrganization(organizationId: string): Promise<ITask[] | null>;
-  findByStatus(status: string, tasks: ITask[]): Promise<ITask[] | null>;
-  findByDate(date: Date, tasks: ITask[]): Promise<ITask[] | null>;
+  findManyByUser(
+    userId: string,
+    status: "Em andamento" | "Concluída" | "Cancelada" | null,
+    date: Date | null
+  ): Promise<ITaskList[]>;
+  findManyByOrganization(
+    organizationId: string,
+    status: "Em andamento" | "Concluída" | "Cancelada" | null,
+    date: Date | null
+  ): Promise<ITaskList[]>;
   assignUserToTask(userId: string, task: ITask): Promise<ITask>;
 }
