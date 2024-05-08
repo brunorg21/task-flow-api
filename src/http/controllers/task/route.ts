7264e52f3f65f3f60cf7386b7cfa4b47768c044e
brunorg21 <rbrunog21@gmail.com>
2024-05-08 14:00:51 -0300

    refactor: change route name

--- src/http/controllers/task/route.ts
+++ src/http/controllers/task/route.ts
@@ -4,12 +4,16 @@ import { create } from "./create";
 import { findById } from "./find-by-id";
 import { edit } from "./edit";
 import { deleteTask } from "./delete";
+import { findManyByUser } from "./find-many-by-user";
+import { findManyByOrganization } from "./find-many-by-organization";
 
 export async function taskRoutes(app: FastifyInstance) {
   app.addHook("onRequest", jwtVerify);
 
-  app.post("/task", create);
-  app.get("/task/:id", findById);
-  app.put("/task/:id", edit);
-  app.delete("/task/:id", deleteTask);
+  app.post("/tasks", create);
+  app.get("/tasks/:id", findById);
+  app.get("/tasks/user", findManyByUser);
+  app.get("/tasks/organization", findManyByOrganization);
+  app.put("/tasks/:id", edit);
+  app.delete("/tasks/:id", deleteTask);
 }
