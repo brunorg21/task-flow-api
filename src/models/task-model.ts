4c6de5840f65f6fbebdbfa886e0cd97433725f21
brunorg21 <rbrunog21@gmail.com>
2024-05-08 13:56:58 -0300

    feat: add new model to task

--- src/models/task-model.ts
+++ src/models/task-model.ts
@@ -1,3 +1,6 @@
+import { IAttachment } from "./attachment-model";
+import { INote } from "./note-model";
+
 export interface ITask {
   id: string;
   title: string;
@@ -7,6 +10,18 @@ export interface ITask {
   assignedId: string | null;
   status?: "Em andamento" | "Concluída" | "Cancelada";
   attachments?: string[] | null;
+}
+
+export interface ITaskList {
+  id: string;
+  title: string;
+  createdAt?: Date | null;
+  userId: string;
+  organizationId: string | null;
+  assignedId: string | null;
+  status?: "Em andamento" | "Concluída" | "Cancelada";
+  attachments?: IAttachment[];
+  note: INote[];
 }
 
 export type ITaskCreate = Omit<ITask, "id">;
