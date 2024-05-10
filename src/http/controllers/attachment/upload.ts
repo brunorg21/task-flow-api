e9ecbe6c0183e55437e04ddc3d0f4aeab6ae41ee
brunorg21 <rbrunog21@gmail.com>
2024-05-09 22:00:27 -0300

    refactor: change upload attachment use case

--- src/http/controllers/attachment/upload.ts
+++ src/http/controllers/attachment/upload.ts
@@ -1,6 +1,9 @@
 import { supabaseClient } from "@/db/supabase/client";
 import { makeCreateAttachmentUseCase } from "@/http/factories/make-create-attachment-use-case";
+import { makeUploadAttachment } from "@/http/factories/make-upload-attachment-use-case";
 import { IAttachmentCreate } from "@/models/attachment-model";
+import { InvalidAttachmentTypeError } from "@/use-cases/@errors/invalid-attachment-type-error";
+import { UploadError } from "@/use-cases/@errors/upload-error";
 import { FastifyRequest, FastifyReply } from "fastify";
 
 export async function upload(req: FastifyRequest, reply: FastifyReply) {
@@ -8,9 +11,13 @@ export async function upload(req: FastifyRequest, reply: FastifyReply) {
   let attachments: IAttachmentCreate[] = [];
   for await (const part of parts) {
     try {
-      const { data } = await supabaseClient.storage
-        .from("attachments")
-        .upload(part.filename, await part.toBuffer());
+      const uploadAttachment = makeUploadAttachment();
+      const buffer = await part.toBuffer();
+      const { data } = await uploadAttachment.execute({
+        buffer,
+        fileName: part.filename,
+        fileType: part.mimetype,
+      });
 
       if (data)
         attachments.push({
@@ -21,6 +28,20 @@ export async function upload(req: FastifyRequest, reply: FastifyReply) {
           url: data.path,
         });
     } catch (error) {
+      if (error instanceof InvalidAttachmentTypeError) {
+        return reply.status(400).send({
+          message: error.message,
+          cause: error.cause,
+        });
+      }
+
+      if (error instanceof UploadError) {
+        return reply.status(406).send({
+          message: error.message,
+          cause: error.cause,
+        });
+      }
+
       return reply
         .status(500)
         .send({ message: "Erro do servidor ao fazer upload" });
