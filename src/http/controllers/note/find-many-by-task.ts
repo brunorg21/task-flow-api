a20c2739ea08029fd01af7dc1164861a612442cc
brunorg21 <rbrunog21@gmail.com>
2024-05-12 16:53:28 -0300

    feat: add find many by task controller

---
+++ src/http/controllers/note/find-many-by-task.ts
@@ -0,0 +1,27 @@
+import { makeFindManyByTaskUseCase } from "@/http/factories/make-find-many-by-task-use-case";
+
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+export async function findManyByTask(req: FastifyRequest, reply: FastifyReply) {
+  const findManyByTaskRequestBodySchema = z.object({
+    taskId: z.string(),
+  });
+
+  const { taskId } = findManyByTaskRequestBodySchema.parse(req.params);
+
+  const findManyByTask = makeFindManyByTaskUseCase();
+
+  try {
+    const notes = await findManyByTask.execute(taskId);
+
+    return reply.status(201).send({
+      notes,
+    });
+  } catch (error) {
+    return reply.status(500).send({
+      message: "Erro interno do servidor",
+      error,
+    });
+  }
+}
