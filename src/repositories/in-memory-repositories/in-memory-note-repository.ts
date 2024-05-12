b0ca791e829528594509a66b91f54c5603acbdb2
brunorg21 <rbrunog21@gmail.com>
2024-05-12 16:58:18 -0300

    remove: remove save attachment while create repository

--- src/repositories/in-memory-repositories/in-memory-note-repository.ts
+++ src/repositories/in-memory-repositories/in-memory-note-repository.ts
@@ -1,12 +1,8 @@
 import { INoteCreate, INote } from "@/models/note-model";
 import { NoteRepository } from "../note-repository";
 import { randomUUID } from "crypto";
-import { IAttachment } from "@/models/attachment-model";
-import { AttachmentRepository } from "../attachment-repository";
 
 export class InMemoryNoteRepository implements NoteRepository {
-  constructor(private attachmentRepository: AttachmentRepository) {}
-
   public items: INote[] = [];
 
   async create(data: INoteCreate): Promise<INote> {
@@ -18,26 +14,6 @@ export class InMemoryNoteRepository implements NoteRepository {
       description: data.description,
       taskId: data.taskId,
     } as INote;
-
-    if (data.attachments) {
-      const attachments = await this.attachmentRepository.findMany(
-        data.attachments
-      );
-
-      const newAttachments = attachments.map((attachment) => {
-        return {
-          id: attachment.id,
-          fileName: attachment.fileName,
-          url: attachment.url,
-          taskId: attachment.taskId,
-          noteId: note.id,
-          createdAt: attachment.createdAt,
-          type: attachment.type,
-        } as IAttachment;
-      });
-
-      await this.attachmentRepository.save(newAttachments);
-    }
 
     this.items.push(note);
 
@@ -64,5 +40,11 @@ export class InMemoryNoteRepository implements NoteRepository {
     const noteIndex = this.items.findIndex((item) => item.id === note.id);
 
     this.items[noteIndex] = note;
+  }
+
+  async findManyByTask(taskId: string): Promise<INote[]> {
+    const notes = this.items.filter((note) => note.taskId === taskId);
+
+    return notes;
   }
 }
