2a9718eb4eb05cdf5449fa6f3d03b707250f8cac
brunorg21 <rbrunog21@gmail.com>
2024-05-12 00:34:02 -0300

    refactor: change dependencies on create organization use case

--- src/use-cases/organization/create.ts
+++ src/use-cases/organization/create.ts
@@ -1,12 +1,19 @@
 import { IOrganizationCreate } from "@/models/organization-model";
 import { OrganizationRepository } from "@/repositories/organization-repository";
+import { UserOrganizationRepository } from "@/repositories/user-organization-repository";
 
 export class CreateOrganizationUseCase {
-  constructor(private organizationRepository: OrganizationRepository) {}
+  constructor(
+    private organizationRepository: OrganizationRepository,
+    private userOrganizationRepository: UserOrganizationRepository
+  ) {}
 
-  async execute(data: IOrganizationCreate) {
+  async execute(data: IOrganizationCreate, userId: string) {
     const org = await this.organizationRepository.create(data);
-
+    await this.userOrganizationRepository.create({
+      organizationId: org.id,
+      userId,
+    });
     return org;
   }
 }
