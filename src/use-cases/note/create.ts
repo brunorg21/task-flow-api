a178b0c051c99d5968ad91c2fddf68c9f27a6557
brunorg21 <rbrunog21@gmail.com>
2024-05-12 16:58:35 -0300

    feat: add save attachment on create use case

--- src/use-cases/note/create.ts
+++ src/use-cases/note/create.ts
@@ -1,12 +1,36 @@
+import { IAttachment } from "@/models/attachment-model";
 import { INoteCreate } from "@/models/note-model";
 import { AttachmentRepository } from "@/repositories/attachment-repository";
 import { NoteRepository } from "@/repositories/note-repository";
 
 export class CreateNoteUseCase {
-  constructor(private noteRepository: NoteRepository) {}
+  constructor(
+    private noteRepository: NoteRepository,
+    private attachmentRepository: AttachmentRepository
+  ) {}
 
   async execute(data: INoteCreate) {
     const note = await this.noteRepository.create(data);
+
+    if (data.attachments) {
+      const attachments = await this.attachmentRepository.findMany(
+        data.attachments as string[]
+      );
+
+      const newAttachments = attachments.map((attachment) => {
+        return {
+          id: attachment.id,
+          fileName: attachment.fileName,
+          url: attachment.url,
+          taskId: attachment.taskId,
+          noteId: note.id,
+          createdAt: attachment.createdAt,
+          type: attachment.type,
+        } as IAttachment;
+      });
+
+      await this.attachmentRepository.save(newAttachments);
+    }
 
     return note;
   }
--- src/use-cases/tasks/create.ts
+++ src/use-cases/tasks/create.ts
@@ -1,10 +1,35 @@
+import { IAttachment } from "@/models/attachment-model";
 import { ITaskCreate } from "@/models/task-model";
+import { AttachmentRepository } from "@/repositories/attachment-repository";
 import { TaskRepository } from "@/repositories/task-repository";
 
 export class CreateTaskUseCase {
-  constructor(private taskRepository: TaskRepository) {}
+  constructor(
+    private taskRepository: TaskRepository,
+    private attachmentRepository: AttachmentRepository
+  ) {}
 
   async execute(data: ITaskCreate) {
-    await this.taskRepository.create(data);
+    const task = await this.taskRepository.create(data);
+
+    if (data.attachments) {
+      const attachments = await this.attachmentRepository.findMany(
+        data.attachments
+      );
+
+      const newAttachments = attachments.map((attachment) => {
+        return {
+          id: attachment.id,
+          fileName: attachment.fileName,
+          url: attachment.url,
+          taskId: task.id,
+          noteId: attachment.noteId,
+          createdAt: attachment.createdAt,
+          type: attachment.type,
+        } as IAttachment;
+      });
+
+      await this.attachmentRepository.save(newAttachments);
+    }
   }
 }
