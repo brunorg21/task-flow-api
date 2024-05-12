efd2142e0e777699eb1b9e58f6f4c454b52df124
brunorg21 <rbrunog21@gmail.com>
2024-05-12 17:00:06 -0300

    feat: add delete and edit note use case

--- src/use-cases/note/delete.ts
+++ src/use-cases/note/delete.ts
@@ -1,14 +1,28 @@
 import { NoteRepository } from "@/repositories/note-repository";
 import { ResourceNotFoundError } from "../@errors/resource-not-found-error";
+import { AttachmentRepository } from "@/repositories/attachment-repository";
 
 export class DeleteNoteUseCase {
-  constructor(private noteRepository: NoteRepository) {}
+  constructor(
+    private noteRepository: NoteRepository,
+    private attachmentRepository: AttachmentRepository
+  ) {}
 
   async execute(id: string) {
     const note = await this.noteRepository.findById(id);
 
     if (!note) {
       throw new ResourceNotFoundError();
+    }
+
+    const attachments = await this.attachmentRepository.findManyByNoteId(
+      note.id
+    );
+
+    if (attachments) {
+      await this.attachmentRepository.deleteMany(
+        attachments.map((attachment) => attachment.id)
+      );
     }
 
     await this.noteRepository.delete(note.id);
