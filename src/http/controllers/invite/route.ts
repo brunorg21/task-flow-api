ca171316908013fc46d85d4cd5772bf0c84a45b5
brunorg21 <rbrunog21@gmail.com>
2024-08-06 17:40:45 -0300

    feat: add invite routes

---
+++ src/http/controllers/invite/route.ts
@@ -0,0 +1,17 @@
+import { jwtVerify } from "@/http/middlewares/jwt-verify";
+import { FastifyInstance } from "fastify";
+import { createInvite } from "./create";
+import { cancelInvite } from "./cancel";
+import { acceptInvite } from "./accept-invite";
+import { findInvitationsReceivedInvite } from "./find-invitations-received";
+import { findInvitationsSendInvite } from "./find-invitations-send";
+
+export async function inviteRoutes(app: FastifyInstance) {
+  app.addHook("onRequest", jwtVerify);
+
+  app.post("/invites", createInvite);
+  app.delete("/invites/:inviteId", cancelInvite);
+  app.patch("/invites/:inviteId", acceptInvite);
+  app.get("/invites/received/:recipientId", findInvitationsReceivedInvite);
+  app.get("/invites/sended/:senderId", findInvitationsSendInvite);
+}
