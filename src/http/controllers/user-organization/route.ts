7ef441b3a65e34803d35c61d279d59502c8fa0c2
brunorg21 <rbrunog21@gmail.com>
2024-08-09 17:19:04 -0300

    refactor: add onRequest hook

--- src/http/controllers/user-organization/route.ts
+++ src/http/controllers/user-organization/route.ts
@@ -1,13 +1,11 @@
 import { FastifyInstance } from "fastify";
 import { create } from "./create";
 import { jwtVerify } from "@/http/middlewares/jwt-verify";
+import { findByOrganization } from "./find-by-organization";
 
 export async function userOrganizationRoutes(app: FastifyInstance) {
-  app.post(
-    "/userOrganization",
-    {
-      onRequest: [jwtVerify],
-    },
-    create
-  );
+  app.addHook("onRequest", jwtVerify);
+
+  app.post("/userOrganization", create);
+  app.get("/userOrganization/:slug", findByOrganization);
 }
