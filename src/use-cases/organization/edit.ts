4ee3121df7c6d0ff9a146660e6a67d5a0bc7c2eb
brunorg21 <rbrunog21@gmail.com>
2024-09-15 22:35:13 -0300

    refactor: create slug and verify if exists org with same name

--- src/use-cases/organization/edit.ts
+++ src/use-cases/organization/edit.ts
@@ -1,6 +1,8 @@
 import { IOrganization } from "@/models/organization-model";
 import { OrganizationRepository } from "@/repositories/organization-repository";
 import { ResourceNotFoundError } from "../@errors/resource-not-found-error";
+import { createSlug } from "@/utils/create-slug";
+import { OrganizationWithSameNameError } from "../@errors/organization-with-same-name-error";
 
 interface EditOrganizationUseCaseRequest {
   organizationId: string;
@@ -15,6 +17,14 @@ export class EditOrganizationUseCase {
 
     if (!org) {
       throw new ResourceNotFoundError();
+    }
+
+    const orgWithSameSlug = await this.organizationRepository.findBySlug(
+      createSlug(name)
+    );
+
+    if (orgWithSameSlug) {
+      throw new OrganizationWithSameNameError();
     }
 
     org.name = name;
